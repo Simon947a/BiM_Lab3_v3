b7e1d8ef54e8ae484ac39d258154d4c58f0da1c4
Simon94 <szymon7a@gmail.com>
2018-04-16 21:08:53 +0200

    ..

--- Assets/Scripts/Pyramid.cs
+++ Assets/Scripts/Pyramid.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pyramid : MonoBehaviour {
+public class Pyramid : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
         int maxHeight = 10;
         SpringJoint spring;
@@ -18,22 +20,63 @@ public class Pyramid : MonoBehaviour {
             {
                 for (int z = -length; z <= length; z++)
                 {
-                    if (Mathf.Abs(x) == length || Mathf.Abs(z) == length)
-                    {
-                        gameObject = Tools.MakeCapsule(x, height, z);
-                        spring = gameObject.AddComponent<SpringJoint>();
-
-                    }
-
+                    gameObject = Tools.MakeCapsule(x - 35, height, z - 50);
+                    gameObject.transform.localScale = new Vector3(1f, 0.5f, 1f);
+                    spring = gameObject.AddComponent<SpringJoint>();
                 }
-                
+
             }
         }
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+}
+public class Tools : MonoBehaviour
+{
+
+    public static GameObject capsulePrefab;
+    public static GameObject capsuleContainer;
+    public static int Count = 0;
+    public static List<GameObject> capsules;
+    public static GameObject cylinder;
+
+    public static GameObject MakeCapsule(float x, float y, float z)
+    {
+        return MakeCapsule(x, y, z, Color.green, 1f);
+    }
+
+    public static GameObject MakeCapsule(float x, float y, float z, Color color, float size)
+    {
+        return MakeCapsule(new Vector3(x, y, z), color, size);
+    }
+
+    private static GameObject GetCubePrefab()
+    {
+        if (capsulePrefab == null)
+            capsulePrefab = Resources.Load("Cylinder") as GameObject;
+        return capsulePrefab;
+    }
+
+    public static GameObject MakeCapsule(Vector3 position, Color color, float size)
+    {
+        Count++;
+        if (capsuleContainer == null)
+        {
+            capsuleContainer = new GameObject("cylinder container");
+            capsules = new List<GameObject>();
+        }
+
+        cylinder = Instantiate(GetCubePrefab()) as GameObject;
+        capsules.Add(cylinder);
+        cylinder.transform.position = position;
+        cylinder.transform.parent = capsuleContainer.transform;
+        cylinder.name = "cylinder " + Count;
+
+        cylinder.GetComponent<Renderer>().material.color = color;
+        cylinder.transform.localScale = new Vector3(size, size, size);
+
+        return cylinder;
+    }
+
+
 }
